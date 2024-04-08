224d68e35e745afc0cd67df2d87e81e6205d0e92
Ksenia Blokhina <kseniablokhina@MacBook-Pro-Ksenia.local>
2024-04-08 12:52:27 +0300

    sheep movement

--- scene/game/tile_map1.gd
+++ scene/game/tile_map1.gd
@@ -3,6 +3,7 @@ class_name GameField
 
 var path = []
 var can_build = false
+var visited = Vector2i(-1,-1)
 
 func _process(delta):
 	if can_build:
@@ -11,9 +12,19 @@ func _process(delta):
 func build_path():
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		var tile = local_to_map(to_local(get_global_mouse_position()))
+		
 		if path.find(tile) == -1:
+			visited = tile
 			path.push_back(tile)
-		set_cell(1, tile, 0, Vector2i(9,7), 0)
+			set_cell(1, tile, 0, Vector2i(9,7), 0)
+		
+		if visited != tile and path.find(tile) != -1:
+			var idx = path.find(tile)
+			for el in range(idx, path.size()):
+				erase_cell(1, path[el])
+			while path.size() != idx:
+				path.remove_at(path.size() - 1)
+		
 	else:
 		if !path.is_empty():
 			await move_by_path.call(path.slice(1))
